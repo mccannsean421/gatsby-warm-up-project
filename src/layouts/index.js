a7b149c15f7c86cf6b9e13d184f1b9f70c414e46
mccannsean421 <mccannsean421@gmail.com>
2017-09-06 21:32:01 +0100

    initial

---
+++ src/layouts/index.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Link from 'gatsby-link'
+import Helmet from 'react-helmet'
+
+//import './index.css'
+import './index.scss'
+
+const Header = () => (
+  <div>
+    <div className="header">
+      <h1 style={{ margin: 0 }}>
+        <Link to="/">
+          Gatsby Tutorial
+        </Link>
+      </h1>
+
+      <ul>
+        <li><Link to="/about">About</Link></li>
+        <li>Posts</li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    </div>
+  </div>
+)
+
+const Footer = () => (
+  <div>
+    <div className="footer">
+      <span>This is the footer.</span>
+    </div>
+  </div>
+)
+
+const TemplateWrapper = ({ children }) => (
+  <div>
+    <Helmet
+      title="Gatsby Default Starter"
+      meta={[
+        { name: 'description', content: 'Sample' },
+        { name: 'keywords', content: 'sample, something' },
+      ]}
+    />
+    <Header />
+    <div>
+      {children()}
+    </div>
+  </div>
+)
+
+TemplateWrapper.propTypes = {
+  children: PropTypes.func,
+}
+
+export default TemplateWrapper
---
+++ src/pages/page-3.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const ThirdPage = () => (
+  <div>
+    <h1>Hi This is the page you added</h1>
+    <p>Welcome to page 2</p>
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+)
+
+export default ThirdPage
